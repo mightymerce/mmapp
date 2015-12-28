f1c3442a092241381a5e9beeda9b25b7dd68a167
mywagi <mwagner@markuss-mbp.speedport.ip>
2015-12-28 23:08:00 +0100

    changed port

--- config/env/default.js
+++ config/env/default.js
@@ -7,7 +7,7 @@ module.exports = {
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.googleAnalyticsTrackingID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
-  port: process.env.PORT || 3001,
+  port: process.env.PORT || 3000,
   templateEngine: 'swig',
   // Session Cookie settings
   sessionCookie: {
