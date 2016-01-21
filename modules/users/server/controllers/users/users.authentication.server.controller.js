179c05acb5950d4d515f289ca43d5f0026abf3dd
mywagi <mwagner@markuss-mbp.speedport.ip>
2016-01-21 10:33:02 +0100

    Delivery set to 1 at registration

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -31,7 +31,7 @@ exports.signup = function (req, res) {
 
   // Add missing user fields
   user.provider = 'local';
-  user.displayName = user.firstName + ' ' + user.lastName;
+  user.tutorialDeliveryDetail = '1';
 
   // Then save the user
   user.save(function (err) {
