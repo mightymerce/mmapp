a7e32917b5e73f9efb8d5c4bd5c9f056cbdc9c58
mywagi <mwagner@markuss-mbp.speedport.ip>
2016-02-26 16:11:11 +0100

    took out display name as it is not shown

--- modules/checkouts/client/controllers/checkouts.client.controller.js
+++ modules/checkouts/client/controllers/checkouts.client.controller.js
@@ -68,7 +68,7 @@ angular.module('checkouts').controller('CheckoutsController', ['$rootScope', '$w
           $(document).ready(function($) {
             // todo - wie need channel Pinterest in URL. Can't distinguish at the moment
 
-            $("meta[name='og:keywords']").attr('content', $scope.product.productTitle + ', ' + $scope.user.displayName + ', shop.mightymerce.com');
+            $("meta[name='og:keywords']").attr('content', $scope.product.productTitle + ', www.shopmightymerce.com');
 
             // FACEBOOK
             $("meta[property='og:site_name']").attr('content', 'shop.mightymerce.com');
