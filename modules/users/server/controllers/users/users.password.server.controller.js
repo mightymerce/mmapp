2bd7b6f3a10f817b8c7c91a7605b2036e7c7a439
mywagi <mwagner@markuss-mbp.speedport.ip>
2016-03-01 18:22:48 +0100

    Forgot password functionality

--- modules/users/server/controllers/users/users.password.server.controller.js
+++ modules/users/server/controllers/users/users.password.server.controller.js
@@ -74,7 +74,7 @@ exports.forgot = function (req, res, next) {
       var mailOptions = {
         to: user.email,
         from: config.mailer.from,
-        subject: 'Password Reset',
+        subject: 'mightymerce - Password Reset',
         html: emailHTML
       };
       smtpTransport.sendMail(mailOptions, function (err) {
